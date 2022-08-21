4e3a052634ab463288f780116789016b7289f72f
Bnt Vince <bnt.vincent31@gmail.com>
2022-08-21 17:29:48 +0200

    Structure backend ok

---
+++ back/controllers/posts.js
@@ -0,0 +1,148 @@
+const fs = require('fs');
+const Posts = {};
+
+exports.getAllPosts = (req, res, next) => {
+    Posts.find()
+        .then((posts) => res.status(200).json(posts))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOnePost = (req, res, next) => {
+    Posts.findOne({ _id: req.params.id })
+        .then(post => res.status(200).json(post))
+        .catch(error => res.status(404).json({ error }));
+};
+
+exports.addNewPost = (req, res, next) => {
+    const postObject = JSON.parse(req.body.post);
+    delete postObject._id;
+    delete postObject.userId;
+    const post = new Posts({
+        ...postObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: []
+    });
+    post.save()
+        .then(() => res.status(201).json({ message: 'Nouvelle posts ajouté !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+const modifyObject = (object, req, res, next) => {
+    Posts.updateOne({ _id: req.params.id }, { ...object, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Post modifié' }))
+        .catch(error => res.status(401).json({ error }));
+};
+
+exports.modifyPost = (req, res, next) => {
+    let postObject
+    if (req.file) {
+        postObject = {
+            ...JSON.parse(req.body.post),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        };
+        delete postObject.userId;
+
+        Posts.findOne({ _id: req.params.id })
+            .then(post => {
+                if (post.userId === req.auth.userId) {
+                    const filename = post.imageUrl.split('/images/')[1];
+                    fs.unlink(`images/${filename}`, () => {
+                        modifyObject(postObject, req, res, next);
+                    });
+
+                } else {
+                    res.status(401).json({ message: 'Opération non-autorisé' })
+                }
+            })
+            .catch(error => res.status(400).json({ error }));
+
+    } else {
+        postObject = {
+            ...req.body
+        };
+        delete postObject.userId;
+
+        Posts.findOne({ _id: req.params.id })
+            .then(post => {
+                if (post.userId === req.auth.userId) {
+                    modifyObject(postObject, req, res, next);
+                } else {
+                    res.status(401).json({ message: 'Opération non-autorisé' })
+                }
+            })
+            .catch(error => res.status(400).json({ error }));
+
+    }
+};
+
+exports.deletePosts = (req, res, next) => {
+
+    Posts.findOne({ _id: req.params.id })
+        .then((post) => {
+            if (post.userId === req.auth.userId) {
+                const filename = post.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Posts.deleteOne({ _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Post supprimé' }))
+                        .catch(error => res.status(400).json({ error }));
+                })
+            } else {
+                res.status(500).json({ message: 'Opération non-autorisé' })
+            }
+        })
+        .catch(error => res.status(400).json({ error }));
+};
+
+const likePost = (object, req, res, next) => {
+    Posts.updateOne({ _id: req.params.id }, { $set: { ...object } })
+        .then(() => {
+            res.status(201).json({ message: 'Like modifié' })
+        })
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.addLikeToAPost = (req, res, next) => {
+    Posts.findOne({ _id: req.params.id })
+        .then(post => {
+            const postObject = post;
+
+            if ((req.body.like === 1 || req.body.like === -1) && (postObject.usersLiked.includes(req.auth.userId) || postObject.usersDisliked.includes(req.auth.userId))) {
+                res.status(400).json({ message: "Opération non-authorisé" })
+            }
+
+            else if (req.body.like === 1) {
+                postObject.usersLiked.push(req.auth.userId);
+                postObject.likes++;
+                likePost(postObject, req, res, next);
+
+            } else if (req.body.like === -1) {
+                postObject.usersDisliked.push(req.auth.userId);
+                postObject.dislikes++;
+                likePost(postObject, req, res, next);
+
+            } else if (req.body.like === 0) {
+                const userIndexInLikes = postObject.usersLiked.indexOf(req.auth.userId)
+                const userIndexInDislikes = postObject.usersDisliked.indexOf(req.auth.userId)
+
+                if (userIndexInLikes > -1) {
+                    postObject.usersLiked.splice(userIndexInLikes, 1);
+                    postObject.likes--;
+                    likePost(postObject, req, res, next);
+                } else if (userIndexInDislikes > -1) {
+                    postObject.usersDisliked.splice(userIndexInDislikes, 1);
+                    postObject.dislikes--;
+                    likePost(postObject, req, res, next);
+                } else {
+                    res.status(400).json({ message: "Erreur inconnu" })
+                }
+            } else {
+                res.status(400).json({ message: "Valeur innatendu" })
+            }
+        })
+        .catch(error => res.status(400).json({ error }));
+};
+
---
+++ back/routes/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const postsCtrl = require('../controllers/posts.js');
+const auth = require('../middleware/auth.js')
+const multer = require('../middleware/multer-config.js')
+
+router.get('/', auth, postsCtrl.getAllPosts);
+router.get('/:id', auth, postsCtrl.getOnePost);
+router.post('/', auth, multer, postsCtrl.addNewPost);
+router.put('/:id', auth, multer, postsCtrl.modifyPost);
+router.delete('/:id', auth, postsCtrl.deletePosts);
+router.post('/:id/like', auth, postsCtrl.addLikeToAPost);
+
+module.exports = router;
